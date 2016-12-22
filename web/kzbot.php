eb0a43443aeb072d025e811f33b3801387357ea4
ktrips <kenichi_yoshida@hotmail.com>
2016-12-23 00:18:41 +0900

    Update kzbot.php

--- web/kzbot.php
+++ web/kzbot.php
@@ -1,94 +1,109 @@
 <?php
 
-require_once('/app/vendor/autoload.php');
-use jp3cki\docomoDialogue\Dialogue;
+#require_once('/app/vendor/autoload.php');
+#use jp3cki\docomoDialogue\Dialogue;
 
 // アカウント情報設定
 $accessToken = getenv('LINE_CHANNEL_ACCESS_TOKEN');
-#$channelId     = getenv('LINE_CHANNEL_ID');
-#$channelSecret = getenv('LINE_CHANNEL_SECRET');
-#$mid           = getenv('LINE_CHANNEL_MID');
 $proxy         = getenv('FIXIE_URL');
 $docomoApiKey  = getenv('DOCOMO_API_KEY');
 $redisUrl      = getenv('REDIS_URL');
 
 // メッセージ受信
-$json_string  = file_get_contents('php://input');
-$json_object  = json_decode($json_string);
-$content      = $json_object->result{0}->content;
-$text         = $content->text;
-$from         = $content->from;
-$message_id   = $content->id;
-$content_type = $content->contentType;
+#$json_string  = file_get_contents('php://input');
+#$json_object  = json_decode($json_string);
+#$content      = $json_object->result{0}->content;
+#$text         = $content->text;
+#$from         = $content->from;
+#$message_id   = $content->id;
+#$content_type = $content->contentType;
 
 // $contextの設定
-$redis   = new Predis\Client($redisUrl);
-$context = $redis->get($from);
+#$redis   = new Predis\Client($redisUrl);
+#$context = $redis->get($from);
 
-$dialog = new Dialogue($docomoApiKey);
+#$dialog = new Dialogue($docomoApiKey);
 
 //Docomo  送信パラメータの準備
-$dialog->parameter->reset();
-$dialog->parameter->utt = $text;
-$dialog->parameter->t = 20;
-$dialog->parameter->context = $context;
-$dialog->parameter->mode = $mode;
+#$dialog->parameter->reset();
+#$dialog->parameter->utt = $text;
+#$dialog->parameter->t = 20;
+#$dialog->parameter->context = $context;
+#$dialog->parameter->mode = $mode;
 
-$ret = $dialog->request();
+#$ret = $dialog->request();
 
-if ($ret === false) {
-    $text = "通信に失敗しました";
+#if ($ret === false) {
+#    $text = "通信に失敗しました";
+#}
+
+#$text = $ret->utt;
+#$redis->set($from, $ret->context);
+
+
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+
+$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+//メッセージ取得
+$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+//ReplyToken取得
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+//メッセージ以外のときは何も返さず終了
+if($type != "text"){
+    exit;
 }
 
-$text = $ret->utt;
-$redis->set($from, $ret->context);
+//docomo返信
+$response = chat($text);
 
-#$headers = array(
-#    "Content-Type: application/json",
-#    "X-Line-ChannelID: {$channelId}",
-#    "X-Line-ChannelSecret: {$channelSecret}",
-#    "X-Line-Trusted-User-With-ACL: {$mid}"
-#);
+//返信データ作成
+$response_format_text = [
+    "type" => "text",
+    "text" => $response
+    ];
 
-#$url = "https://trialbot-api.line.me/v1/events";
+$post_data = [
+    "replyToken" => $replyToken,
+    "messages" => [$response_format_text]
+    ];
 
-$post = <<< EOM
-{
-    "to":["{$from}"],
-    "toChannel":1383378250,
-    "eventType":"138311608800106203",
-    "content":{
-        "toType":1,
-        "contentType":1,
-        "text": "{$text}"
-    }
-}
-EOM;
-
-#$post_data = [
-#	"replyToken" => $replyToken,
-#	"messages" => [$response_format_text]
-#	];
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-#curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-
-#$curl = curl_init($url);
-#curl_setopt($curl, CURLOPT_POST, true);
-#curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-#curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//プロキシ経由フラグ
-curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
-//プロキシ設定
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-#$output = curl_exec($curl);
-
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
     'Authorization: Bearer ' . $accessToken
     ));
 $result = curl_exec($ch);
 curl_close($ch);
+
+#$docomoApiKey  = getenv('DOCOMO_API_KEY');
+
+//ドコモの雑談APIから雑談データを取得
+function chat($text) {
+    // docomo chatAPI
+    #$api_key = '【docomoのAPI Keyを使用する】';
+    $docomoApiKey  = getenv('DOCOMO_API_KEY');
+    $api_url = sprintf('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s', $docomoApiKey);
+    $req_body = array('utt' => $text);
+
+    $headers = array(
+        'Content-Type: application/json; charset=UTF-8',
+    );
+    $options = array(
+        'http'=>array(
+            'method'  => 'POST',
+            'header'  => implode("\r\n", $headers),
+            'content' => json_encode($req_body),
+            )
+        );
+    $stream = stream_context_create($options);
+    $res = json_decode(file_get_contents($api_url, false, $stream));
+
+    return $res->utt;
+}
