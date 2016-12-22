0cca5e5f2fd70f95e15882bee2ca6eee314a06ba
ktrips <kenichi_yoshida@hotmail.com>
2016-12-23 01:03:14 +0900

    Update kzbot.php

--- web/kzbot.php
+++ web/kzbot.php
@@ -1,50 +1,40 @@
 <?php
-$accessToken = 'Qw0I9IoxJ9S6QZRYXu7MF/onE3fSaGXjOZ5X9o8NjJUXqgDmbgj7pE8e8GY2RPzX7qJnOeVNkR+lm7SVpeaJroSiaV5clYnZ7fGadSn0j8OyqSp3prt7MjeWET4NB+N1LcnVCxe0A4IefmvRgjyQVgdB04t89/1O/w1cDnyilFU=';
 
-//$accessToken = '【Cannel Access Tokenの値を使用する】';
-
-//ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
-$jsonObj = json_decode($json_string);
+$json_object = json_decode($json_string);
 
-$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
-//メッセージ取得
-$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
-//ReplyToken取得
-$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
-
-//メッセージ以外のときは何も返さず終了
-if($type != "text"){
-    exit;
+foreach ($json_object->events as $event) {
+    if('message' == $event->type){
+        api_post_request($event->replyToken, $event->message->text);
+    }else if('beacon' == $event->type){
+        api_post_request($event->replyToken, 'BEACONイベント!!');
+    }
 }
 
-//docomo返信
-$response = chat($text);
+function api_post_request($token, $message) {
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $channel_access_token = 'Qw0I9IoxJ9S6QZRYXu7MF/onE3fSaGXjOZ5X9o8NjJUXqgDmbgj7pE8e8GY2RPzX7qJnOeVNkR+lm7SVpeaJroSiaV5clYnZ7fGadSn0j8OyqSp3prt7MjeWET4NB+N1LcnVCxe0A4IefmvRgjyQVgdB04t89/1O/w1cDnyilFU=';
+    $headers = array(
+        'Content-Type: application/json',
+        "Authorization: Bearer {$channel_access_token}"
+    );
+    $post = array(
+        'replyToken' => $token,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $message
+            )
+        )
+    );
 
-//返信データ作成
-$response_format_text = [
-    "type" => "text",
-    "text" => $response
-    ];
-
-$post_data = [
-    "replyToken" => $replyToken,
-    "messages" => [$response_format_text]
-    ];
-
-$ch = curl_init("https://api.line.me/v2/bot/message/reply");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $accessToken
-    ));
-$result = curl_exec($ch);
-curl_close($ch);
-
-
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
+}
 
 //ドコモの雑談APIから雑談データを取得
 function chat($text) {
