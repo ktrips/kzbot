9ba6abe0741fcd2c9845669b32e70e1ca70697ae
ktrips <kenichi_yoshida@hotmail.com>
2016-12-28 18:09:24 +0900

    Update kzbot.php

--- web/kzbot.php
+++ web/kzbot.php
@@ -8,8 +8,14 @@ foreach ($json_object->events as $event) {
     $from = $event->message->from;
     $event_message = $event->message->text;
     $context = $redis->get($from);
+    $response = chat($text, $context);
+    // save context to Redis
+    $redis->set($from, $response->context);
+    
+    $res_content = $event->message;
+    $event_message = $response
     if('message' == $event->type){
-        api_post_request($event->replyToken, chat($text));//$event->message->text);
+        api_post_request($event->replyToken, $response);//chat($text));//$event->message->text);
     }else if('beacon' == $event->type){
         api_post_request($event->replyToken, 'BEACONが近くに来たよ！');
     }
@@ -44,12 +50,16 @@ function api_post_request($token, $message) {
 }
 
 //ドコモの雑談APIから雑談データを取得
-function chat($text) {
+function chat($text, $context) {
     // docomo chatAPI
     $api_key = getenv('DOCOMO_API_KEY');
     //$api_key = '【docomoのAPI Keyを使用する】';
     $api_url = sprintf('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s', $api_key);
-    $req_body = array('utt' => $text);
+    $req_body = array(
+        'utt' => $text
+        'context' => $context,
+    );
+    $req_body['context'] = $text;
 
     $headers = array(
         'Content-Type: application/json; charset=UTF-8',
