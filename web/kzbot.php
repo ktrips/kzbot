6bb944f99afae2bab508344b1b804516ecbc0657
ktrips <kenichi_yoshida@hotmail.com>
2016-12-29 14:28:31 +0900

    Update kzbot.php

--- web/kzbot.php
+++ web/kzbot.php
@@ -1,4 +1,5 @@
 <?php
+require('../vendor/autoload.php');
 //require_once __DIR__.'/vendor/autoload.php';
 $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
