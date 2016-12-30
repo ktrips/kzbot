dd89700ed2c96916d659d046b64f5f6d51ad05cf
ktrips <kenichi_yoshida@hotmail.com>
2016-12-30 20:43:39 +0900

    Update kzbot.php

--- web/kzbot.php
+++ web/kzbot.php
@@ -36,7 +36,7 @@ $redisUrl = getenv('REDIS_URL');
 
 foreach ($json_object->events as $event) {
     // get context from Redis
-    //$redis = new Predis\Client(getenv('REDIS_URL'));
+    $redis = new Predis\Client(getenv('REDIS_URL'));
     $content=$event->message;
     $type  = $event->message->type;
     $from  = $event->message->from;
